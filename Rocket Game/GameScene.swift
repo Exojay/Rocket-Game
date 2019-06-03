0bc87bc3024b36ed545d7d966649926b8d387b6c
Carlos Conley <carlosconley@icloud.com>
2019-06-03 11:48:37 -0500

    second commit

--- Rocket Game/GameScene.swift
+++ Rocket Game/GameScene.swift
@@ -9,81 +9,121 @@
 import SpriteKit
 import GameplayKit
 
+var asteroids:[SKSpriteNode] = [SKSpriteNode(),SKSpriteNode(),SKSpriteNode(),SKSpriteNode(),SKSpriteNode(),SKSpriteNode()]
+
+/*var asteroids = [
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	[SKSpriteNode(), CGFloat(CGFloat.random(in: 480...1000)), CGFloat(CGFloat.random(in: -640...640))],
+	]
+*/
+var a_vel:CGFloat = -6.0
+var gameOver:Bool = true
+
+var score:Int = 0
+let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+	score += 1
+	
+}
+
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+	
+	var label = SKLabelNode()
+	var scoreLabel = SKLabelNode()
+	var player = SKSpriteNode()
+	var button = SKNode()
+	var background = SKSpriteNode()
+	
+	
+	
+	override func didMove(to view: SKView) {
+		
+		player = self.childNode(withName: "player") as! SKSpriteNode
+		background = self.childNode(withName: "background") as! SKSpriteNode
+		scoreLabel = self.childNode(withName: "scoreLabel") as! SKLabelNode
+		label = self.childNode(withName: "startLabel") as! SKLabelNode
+		button = self.childNode(withName: "startButton") as! SKNode
+		for i in 0...(asteroids.count - 1) {
+			asteroids[i] = self.childNode(withName: "asteroid" + String(i)) as! SKSpriteNode
+		}
+		
+		
+	}
+
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for touch in touches {
+			
+			let location = touch.location(in: self)
+			
+			if (location.y >= button.position.y - 50) && (location.y <= button.position.y + 50) && (location.x >= button.position.x - 50) && (location.x <= button.position.x + 50) && gameOver == true {
+				gameOver = false
+				for i in 0...(asteroids.count - 1) {
+					asteroids[i].run(SKAction.unhide())
+				}
+				player.run(SKAction.unhide())
+				button.run(SKAction.hide())
+				label.run(SKAction.hide())
+				label.text = "Game Over"
+				score = 0;
+			}
+			player.run(SKAction.moveTo(y: location.y, duration: 0.1))
+			player.run(SKAction.moveTo(x: location.x, duration: 0.1))
+			
+		}
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for touch in touches {
+			
+			let location = touch.location(in: self)
+			
+			player.run(SKAction.moveTo(y: location.y, duration: 0.1))
+			player.run(SKAction.moveTo(x: location.x, duration: 0.1))
+			
+		}
+	}
+	
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-    }
+		if gameOver == false {
+			scoreLabel.text = "Score: " + String(score)
+			timer
+			player.run(SKAction.unhide())
+			for i in 0...(asteroids.count - 1) {
+				
+				asteroids[i].run(SKAction.moveBy(x: a_vel, y: 0, duration: 0))
+
+				if asteroids[i].position.x < CGFloat(-470) {
+					asteroids[i].position.x = CGFloat.random(in: 480 ... 900)
+					asteroids[i].position.y = CGFloat.random(in: -640...640)
+				}
+
+				if (player.position.x + 25 >= asteroids[i].position.x - 50) && (player.position.x - 25 <= asteroids[i].position.x + 50) && (player.position.y + 50 >= asteroids[i].position.y - 50) && (player.position.y - 50 <= asteroids[i].position.y + 50) {
+					gameOver = true
+					
+					
+					player.run(SKAction.hide())
+					button.run(SKAction.unhide())
+					label.run(SKAction.unhide())
+					
+					for i in 0...(asteroids.count - 1) {
+						asteroids[i].run(SKAction.hide())
+						asteroids[i].position.x = CGFloat.random(in: 480...1000)
+						asteroids[i].position.y = CGFloat.random(in: -640...640)
+					}
+					
+	
+				}
+			}
+			background.run(SKAction.moveBy(x: a_vel * 0.5, y: 0, duration: 0))
+			if background.position.x < CGFloat(-1709) {
+				background.position.x = 1709
+			}
+		}
+	}
 }
+
